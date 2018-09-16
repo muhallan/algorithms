ce69c14b3a03b5fd73f67bbccdd849e170c1497e
Muhwezi Allan <allan.muhwezi@andela.com>
2018-09-16 19:11:35 +0300

    add graphs implementation and README

---
+++ data_structures/graphs/README.md
@@ -0,0 +1,54 @@
+# Graphs
+A graph is a data structure that represents sets of objects where some pairs of objects are
+connected by links. The interconnected objects are represented by points called `vertices` and the
+links that connect the `vertices` are called `edges`. An edge connects two vertices to show that
+there is a relationship between them. Edges may be one-way ot two-way. If the edges in a graph are
+all one-way, that graph is a `directed graph` or a `digraph`. Edges may contain a `weight` that
+indicates the cost to go from one vertex to another. A `path` in a graph is a sequence of vertices
+that are connected by edges. A `cycle` in a directed graph is a path that starts and ends at the same
+vertex. A graph with no cycles is called an `acyclic graph`. A directed graph with no cycles is called a
+`directed acyclic graph (DAG)`.
+
+A graph is commonly represented as an `adjacency matrix` and an `adjacency list`.
+
+1. Adjacency Matrix
+
+   This uses a two-dimensional matrix to represent a graph. In this representation, each of the rows
+   and columns represent a vertex in the graph. If it's a weighted graph, the value that is stored in
+   the cell which is the intersection of row v and column w indicates the weight of the edge from vertex
+   v to vertex w. For non-weighted graphs, presence of a vertex can be shown by filling the intersection cell
+   of a row and column by 1 and leaving the rest as 0.
+
+   The adjacency matrix can be represent as a 2D array of size V * V where V is the number of vertices in
+   the graph.
+
+   The advantage of an adjacency matrix is that it is simple to implement and follow. For smaller graphs, it
+   is easy to see which nodes are connected to other nodes.
+
+   Its disadvantage is that it consumes a lot of space which is **O(V^2)**. Even when the graph has few edges
+   (sparse), it still consumes the same space.
+1. Adjacency List
+
+   In this implementation, a graph is represented as a dictionary of vertices where the key of each vertex is its name
+   and the value is the list of other vertices that it's connected to. In case it's a weighted graph, the vertices can be
+   represented as a pair, making it a list of pairs or a list of dictionaries in Python where the keys are the
+   vertices and the values are the weights. The example code uses an adjacency list.
+
+   The size of the dictionary is equal to the number of the vertices in the graph.
+
+   The advantage of an adjacency list is that it saves space, and it compactly represents a sparse graph.
+   We can also easily find all the links that are directly connected to a particular vertex.
+
+
+## Applications
+Social network graphs. Graphs are used to represent relationships between users in social network sites
+like Facebook, Twitter, Instagram or LinkedIn. They are used to show users that are communicating with each other,
+those that influence each other or even if users know each other.
+
+Maps applications. Graphs are used by apps such as Google Maps, Bing maps to find the shortest routes between
+locations or getting to know traffic patterns. Intersections of roads are considered as vertices and the roads
+connecting the vertices are the edges.
+
+In computer science, graphs are used to solve a number of problems by representing flow of
+information.
+
---
+++ data_structures/graphs/graph.py
@@ -0,0 +1,132 @@
+class Vertex:
+	"""
+	This class represents each vertex in the graph.
+	Each vertex uses a dictionary to keep track of the vertices to which it is
+	connected to, and the weight of each edge
+	"""
+	def __init__(self, key):
+		"""
+		Initialize a vertex with a given id which is the key by which it is
+		referred.
+		:param key: string. The name of the vertex
+		"""
+		self.id = key
+		self.connected_to = {}
+
+	def add_neighbor(self, neighbor, weight=0):
+		"""
+		Used to add a connection from this vertex to another vertex
+		represented by neighbor.
+		:param neighbor: Vertex. The neighboring vertex to connect to
+		:param weight: int. The weight of the vertex to connect to
+		"""
+		self.connected_to[neighbor] = weight
+
+	def get_connections(self):
+		"""
+		Returns all the vertices this vertex is connected to
+		:return: list. List of the connected vertices as represented by the
+		connected_to instance variable
+		"""
+		return self.connected_to.keys()
+
+	def get_id(self):
+		"""
+		Get the id of the vertex
+		:return: str. The id of the vertex
+		"""
+		return self.id
+
+	def get_weight(self, neighbor):
+		"""
+		Get the weight of the given neighboring vertex
+		:param neighbor: Vertex. The neighboring vertex whose weight is required
+		:return: int. The weight of the provided neighbor
+		"""
+		return self.connected_to[neighbor]
+
+	def __str__(self):
+		return str(self.id) + ' connected_to: ' + str([x.id for x in self.connected_to])
+
+
+class Graph:
+	"""
+	This class represents the whole graph. It holds the dictionary of vertices
+	that maps vertex names to vertex objects
+	"""
+	def __init__(self):
+		self.vertex_list = {}
+		self.num_of_vertices = 0
+
+	def add_vertex(self, key):
+		"""
+		Add a new vertex to the graph
+		:param key: str. The name of the vertex to be added
+		:return: Vertex. The newly created vertex object
+		"""
+		self.num_of_vertices = self.num_of_vertices + 1
+		new_vertex = Vertex(key)
+		self.vertex_list[key] = new_vertex
+		return new_vertex
+
+	def get_vertex(self, key):
+		"""
+		Get the Vertex represented by the given key
+		:param key: str. The name of the vertex to return
+		:return: Vertex. Get the vertex object represented by the given key
+		if it's available, else return None
+		"""
+		return self.vertex_list[key] if key in self.vertex_list else None
+
+	def add_edge(self, vertex_key_one, vertex_key_two, weight=0):
+		"""
+		Add a connection/edge between two vertices whose keys are supplied
+		and give it the weight provided
+		:param vertex_key_one: str. The key of vertex to add an edge to
+		:param vertex_key_two: str. The key of the vertex to connect to
+		:param weight: int. The weight to give to the edge
+		"""
+		if vertex_key_one not in self.vertex_list:
+			self.add_vertex(vertex_key_one)
+		if vertex_key_two not in self.vertex_list:
+			self.add_vertex(vertex_key_two)
+		self.vertex_list[vertex_key_one].add_neighbor(self.vertex_list[vertex_key_two], weight)
+
+	def get_vertices(self):
+		"""
+		Get all the vertices in the graph
+		:return: list. The list of all the keys of the vertices in the graph
+		"""
+		return self.vertex_list.keys()
+
+	def __contains__(self, key):
+		return key in self.vertex_list
+
+	def __iter__(self):
+		return iter(self.vertex_list.values())
+
+
+# testing
+graph = Graph()
+for i in range(6):
+	graph.add_vertex(i)
+
+graph.add_edge(0, 1, 5)
+graph.add_edge(0, 5, 2)
+graph.add_edge(1, 2, 4)
+graph.add_edge(2, 3, 9)
+graph.add_edge(3, 4, 7)
+graph.add_edge(3, 5, 3)
+graph.add_edge(4, 0, 1)
+graph.add_edge(5, 4, 8)
+graph.add_edge(5, 2, 1)
+
+# vertices in the graph
+print(list(graph.get_vertices()))
+
+# number of vertices
+print(graph.num_of_vertices)
+
+# each vertex
+for vertex in graph:
+	print(vertex)
